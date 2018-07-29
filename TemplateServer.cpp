403547550c1d8ac8ef60ec75c813bd1d198cb0c6
Arves100 <arves100@gmail.com>
2018-07-29 15:07:39 +0200

    Added error checks

--- TemplateServer.cpp
+++ TemplateServer.cpp
@@ -46,27 +46,45 @@ bool MDKDLLAPI CTemplateServer::Bind(const char *ip, int port, bool udp)
 	if (udp)
 	{
 		real_udp_socket = (uv_udp_t*)malloc(sizeof uv_udp_t);
-		uv_udp_init((uv_loop_t*)m_loop, real_udp_socket);
+		r = uv_udp_init((uv_loop_t*)m_loop, real_udp_socket);
 	}
 	else
 	{
 		real_tcp_socket = (uv_tcp_t*)malloc(sizeof uv_tcp_t);
-		uv_tcp_init((uv_loop_t*)m_loop, real_tcp_socket);
+		r = uv_tcp_init((uv_loop_t*)m_loop, real_tcp_socket);
+	}
+
+	if (r)
+	{
+		LOG_ERROR("Server", "%s listen error: %s\n", udp ? "UDP" : "TCP", uv_strerror(r));
+		return false;
 	}
 
 	// Resolve ip and port
 	uv_ip4_addr(ip, port, &addr);
 
 	if (udp)
-		uv_udp_bind(real_udp_socket, (const struct sockaddr*)&addr, 0);
+		r = uv_udp_bind(real_udp_socket, (const struct sockaddr*)&addr, 0);
 	else
-		uv_tcp_bind(real_tcp_socket, (const struct sockaddr*)&addr, 0);
+		r = uv_tcp_bind(real_tcp_socket, (const struct sockaddr*)&addr, 0);
+
+	if (r)
+	{
+		LOG_ERROR("Server", "%s listen error: %s\n", udp ? "UDP" : "TCP", uv_strerror(r));
+		return false;
+	}
 
 	/* Listen the socket */
 	if (udp)
 		r = uv_udp_recv_start((uv_udp_t*)real_tcp_socket, libuv_callback_allocate_buffer, libuv_callback_on_server_udp_new_connection);
 	else
 		r = uv_listen((uv_stream_t*)real_tcp_socket, DEFAULT_BACKLOG, libuv_callback_on_server_tcp_new_connection);
+
+	if (r)
+	{
+		LOG_ERROR("Server", "%s listen error: %s\n", udp ? "UDP" : "TCP", uv_strerror(r));
+		return false;
+	}
 
 	// Access the loop from the other functions
 	if (udp)
