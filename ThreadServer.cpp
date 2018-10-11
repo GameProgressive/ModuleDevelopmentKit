1157141f1b0f29be01afa06b930265237911b7ad
Arves100 <arves100@gmail.com>
2018-10-11 14:52:02 +0000

    Fixed exporting in threadserver

--- ThreadServer.cpp
+++ ThreadServer.cpp
@@ -12,6 +12,7 @@
 	You should have received a copy of the GNU Lesser General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#define MDK_EXPORT_FUNCTIONS 1 /*Export the methods*/
 #include "ThreadServer.h"
 #include "ModuleEntryPoint.h"
 
